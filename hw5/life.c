40accffdb0b2fa3f730331e80331a18d2f6a9a42
Ashkan <ashkan.pk@gmail.com>
2014-12-03 06:40:48 -0500

    Copied sequential gol, moved a couple statements out of inner loop. 32% speedup.

--- hw5/life.c
+++ hw5/life.c
@@ -12,12 +12,65 @@
 /*****************************************************************************
  * Game of life implementation
  ****************************************************************************/
-char*
+
+#define SWAP_BOARDS( b1, b2 )  do { \
+  char* temp = b1; \
+  b1 = b2; \
+  b2 = temp; \
+} while(0)
+
+#define BOARD( __board, __i, __j )  (__board[(__i) + LDA*(__j)])
+
+
+    char*
 game_of_life (char* outboard, 
-	      char* inboard,
-	      const int nrows,
-	      const int ncols,
-	      const int gens_max)
+        char* inboard,
+        const int nrows,
+        const int ncols,
+        const int gens_max)
 {
-  return sequential_game_of_life (outboard, inboard, nrows, ncols, gens_max);
+    /* HINT: in the parallel decomposition, LDA may not be equal to
+       nrows! */
+    const int LDA = nrows;
+    int curgen, i, j;
+
+    for (curgen = 0; curgen < gens_max; curgen++)
+    {
+        /* HINT: you'll be parallelizing these loop(s) by doing a
+           geometric decomposition of the output */
+        for (i = 0; i < nrows; i++)
+        {
+            const int inorth = mod (i-1, nrows);
+            const int isouth = mod (i+1, nrows);
+            for (j = 0; j < ncols; j++)
+            {
+                const int jwest = mod (j-1, ncols);
+                const int jeast = mod (j+1, ncols);
+
+                const char neighbor_count = 
+                    BOARD (inboard, inorth, jwest) + 
+                    BOARD (inboard, inorth, j) + 
+                    BOARD (inboard, inorth, jeast) + 
+                    BOARD (inboard, i, jwest) +
+                    BOARD (inboard, i, jeast) + 
+                    BOARD (inboard, isouth, jwest) +
+                    BOARD (inboard, isouth, j) + 
+                    BOARD (inboard, isouth, jeast);
+
+                BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
+
+            }
+        }
+        SWAP_BOARDS( outboard, inboard );
+
+    }
+    /* 
+     * We return the output board, so that we know which one contains
+     * the final result (because we've been swapping boards around).
+     * Just be careful when you free() the two boards, so that you don't
+     * free the same one twice!!! 
+     */
+    return inboard;
 }
+
+
